c418a8956ae1eec6b7d7743104af529f3d1793a3
VN <novictan15@gmail.com>
2022-09-14 21:01:54 +0300

    My second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,7 @@ public class Main{
         System.out.println("f = " + f);
         System.out.println("i = " + i);
         System.out.println("y = " + y);
+        system.out.println();
     }
 }
 
