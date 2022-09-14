1e595ff9203463cf776bc82d812857f603233534
VN <novictan15@gmail.com>
2022-09-14 21:41:36 +0300

    3 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 import java.util.*;
 public class Main{
     public static void main(String[] args) {
-        char c2;
+        char c2 = 'A';
         byte b;
         float f;
         int i = -500;
@@ -15,7 +15,8 @@ public class Main{
         System.out.println("f = " + f);
         System.out.println("i = " + i);
         System.out.println("y = " + y);
-        system.out.println();
+        System.out.println(c2);
+        System.out.println();
     }
 }
 
