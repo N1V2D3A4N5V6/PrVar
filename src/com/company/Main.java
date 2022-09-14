58815c5d95e11636feaa88ed45b7c2ba5f2f90eb
VN <novictan15@gmail.com>
2022-09-14 20:41:34 +0300

    My first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        char c2;
+        byte b;
+        float f;
+        int i = -500;
+        f = 1 + new Random().nextFloat() * (1000000 - 1);
+        i = Math.round(f);
+        b = (byte)i;
+        double y = Math.pow(i, 5) / Math.log(f) - Math.sin(f) / b;
+
+        System.out.println("b = " + b);
+        System.out.println("f = " + f);
+        System.out.println("i = " + i);
+        System.out.println("y = " + y);
+    }
+}
+
